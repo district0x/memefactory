1dacce38eca0c3ae9a167bc64e6d2fa055c834d5
fbielejec <fbielejec@gmail.com>
2018-06-20 18:44:27 +0200

    Fix event propagation bug. Id needs to be passed.

--- src/memefactory/ui/components/tiles.cljs
+++ src/memefactory/ui/components/tiles.cljs
@@ -9,11 +9,12 @@
             [clojure.string :as str]
             [district.format :as format]))
 
-(defn flippable-tile [{:keys [:front :back :id]}]
+(defn flippable-tile [{:keys [front back id]
+                       :or {id (str (random-uuid))}}]
   (let [this (r/current-component)
         flipped? (r/atom false)]
     (fn [{:keys [:front :back]}]
-      [:div.container {:id id
+      [:div.container {:id (look id)
                        :style {:width 300 :height 380 :background-color :orange :margin 5}
                        :on-click (fn [event]
                                    (when (= id (-> event
