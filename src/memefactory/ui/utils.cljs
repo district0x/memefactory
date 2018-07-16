a5a45a0ed894c85accb5682fcf30cbbcf40ca3ab
filip <fbielejec@gmail.com>
2018-07-16 19:51:32 +0200

    GraphQL Date to JS/Date parser

--- src/memefactory/ui/utils.cljs
+++ src/memefactory/ui/utils.cljs
@@ -1,6 +1,12 @@
 (ns memefactory.ui.utils
-  (:require [district.ui.router.utils :as router-utils]
+  (:require [cljs-time.coerce :as time-coerce]
+            [district.ui.router.utils :as router-utils]
             [cemerick.url :as url]))
+
+(defn gql-date->date
+  "parse GraphQL Date type as JS Date object ready to be formatted"
+  [gql-date]
+  (time-coerce/from-long (* 1000 gql-date)))
 
 (defn path [& args]
   (str "#" (apply router-utils/resolve args)))
