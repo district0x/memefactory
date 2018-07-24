3def2d89813061d1d0ec2117494b409964aa9704
filip <fbielejec@gmail.com>
2018-07-24 22:17:30 +0200

    checkout travis build status fix from master

--- test/memefactory/tests/runner.cljs
+++ test/memefactory/tests/runner.cljs
@@ -1,9 +1,9 @@
 (ns memefactory.tests.runner
   (:require [cljs.nodejs :as nodejs]
-            [cljs.test :refer [run-tests]]
             [district.graphql-utils :as graphql-utils]
             [district.server.graphql :as graphql]
             [district.server.graphql.utils :as utils]
+            [doo.runner :refer-macros [doo-tests]]
             [memefactory.server.graphql-resolvers :refer [resolvers-map]]
             [memefactory.shared.graphql-schema :refer [graphql-schema]]
             [memefactory.tests.graphql-resolvers.graphql-resolvers-tests]
@@ -16,6 +16,8 @@
 
 (nodejs/enable-util-print!)
 
+(set! (.-error js/console) (fn [x] (.log js/console x)))
+
 (defn on-jsload []
   (graphql/restart {:schema (utils/build-schema graphql-schema
                                                 resolvers-map
@@ -23,15 +25,11 @@
                                                  :gql-name->kw graphql-utils/gql-name->kw})
                     :field-resolver (utils/build-default-field-resolver graphql-utils/gql-name->kw)}))
 
-(set! (.-error js/console) (fn [x] (.log js/console x)))
-
-(defn -main [& _]
-  (run-tests 'memefactory.tests.smart-contracts.deployment-tests
-             'memefactory.tests.smart-contracts.meme-auction-tests
-             'memefactory.tests.smart-contracts.param-change-tests
-             'memefactory.tests.smart-contracts.registry-entry-tests
-             'memefactory.tests.smart-contracts.registry-tests
-             'memefactory.tests.smart-contracts.meme-tests
-             'memefactory.tests.graphql-resolvers.graphql-resolvers-tests))
-
-(set! *main-cli-fn* -main)
+(doo-tests
+ 'memefactory.tests.smart-contracts.deployment-tests
+ 'memefactory.tests.smart-contracts.meme-auction-tests
+ 'memefactory.tests.smart-contracts.param-change-tests
+ 'memefactory.tests.smart-contracts.registry-entry-tests
+ 'memefactory.tests.smart-contracts.registry-tests
+ 'memefactory.tests.smart-contracts.meme-tests
+ 'memefactory.tests.graphql-resolvers.graphql-resolvers-tests)
