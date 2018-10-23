8f1388aaf2b309f108eca5e44be0ce0cbc74b32a
Juan Monetta <jpmonettas@gmail.com>
2018-10-23 17:29:12 -0300

    Fix compilation errors

--- src/memefactory/server/contract/meme_auction_factory.cljs
+++ src/memefactory/server/contract/meme_auction_factory.cljs
@@ -4,8 +4,21 @@
 (defn construct [{:keys [:meme-token]} & [opts]]
   (contract-call [:meme-auction-factory :meme-auction-factory-fwd] :construct meme-token (merge {:gas 200000} opts)))
 
-(defn meme-auction-event-in-tx [tx-hash & args]
-  (apply contract-event-in-tx tx-hash [:meme-auction-factory :meme-auction-factory-fwd] :MemeAuctionEvent args))
+(defn meme-auction-started-event [& args]
+  (apply contract-call [:meme-auction-factory :meme-auction-factory-fwd] :MemeAuctionStartedEvent args))
 
-(defn meme-auction-event [& args]
-  (apply contract-call [:meme-auction-factory :meme-auction-factory-fwd] :MemeAuctionEvent args))
+(defn meme-auction-buy-event [& args]
+  (apply contract-call [:meme-auction-factory :meme-auction-factory-fwd] :MemeAuctionBuyEvent args))
+
+(defn meme-auction-canceled-event [& args]
+  (apply contract-call [:meme-auction-factory :meme-auction-factory-fwd] :MemeAuctionCanceledEvent args))
+
+
+(defn meme-auction-started-event-in-tx [tx-hash & args]
+  (apply contract-event-in-tx tx-hash [:meme-auction-factory :meme-auction-factory-fwd] :MemeAuctionStartedEvent args))
+
+(defn meme-auction-buy-event-in-tx [tx-hash & args]
+  (apply contract-event-in-tx tx-hash [:meme-auction-factory :meme-auction-factory-fwd] :MemeAuctionBuyEvent args))
+
+(defn meme-auction-canceled-event-in-tx [tx-hash & args]
+  (apply contract-event-in-tx tx-hash [:meme-auction-factory :meme-auction-factory-fwd] :MemeAuctionCanceledEvent args))
