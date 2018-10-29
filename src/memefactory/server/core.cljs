316f6917be9e5069d1e461d0e9d9a930fd6eb32d
fbielejec <fbielejec@gmail.com>
2018-10-29 21:11:53 +0100

    core ns

--- src/memefactory/server/core.cljs
+++ src/memefactory/server/core.cljs
@@ -1,57 +1,57 @@
 (ns memefactory.server.core
   (:require
-    [cljs.nodejs :as nodejs]
-    [district.server.config :refer [config]]
-    [district.server.logging]
-    [district.server.middleware.logging :refer [logging-middlewares]]
-    [district.server.web3-watcher]
-    [memefactory.server.db]
-    [memefactory.server.deployer]
-    [memefactory.server.emailer]
-    [memefactory.server.generator]
-    [memefactory.server.ipfs]
-    [memefactory.server.syncer]
-    [memefactory.server.ranks-cache]
-    [memefactory.shared.smart-contracts]
-    [mount.core :as mount]
-    [taoensso.timbre :refer-macros [info warn error]]
-    [cljs-time.core :as t]
-    [district.server.graphql :as graphql]
-    [district.server.graphql.utils :as utils]
-    [district.graphql-utils :as graphql-utils]
-    [memefactory.shared.graphql-schema :refer [graphql-schema]]
-    [memefactory.server.graphql-resolvers :refer [resolvers-map]]))
+   [cljs-time.core :as t]
+   [cljs.nodejs :as nodejs]
+   [district.graphql-utils :as graphql-utils]
+   [district.server.config :refer [config]]
+   [district.server.graphql :as graphql]
+   [district.server.graphql.utils :as utils]
+   [district.server.logging]
+   [district.server.middleware.logging :refer [logging-middlewares]]
+   [district.server.web3-watcher]
+   [memefactory.server.db]
+   [memefactory.server.deployer]
+   [memefactory.server.emailer]
+   [memefactory.server.generator]
+   [memefactory.server.graphql-resolvers :refer [resolvers-map]]
+   [memefactory.server.ipfs]
+   [memefactory.server.ranks-cache]
+   [memefactory.server.syncer]
+   [memefactory.shared.graphql-schema :refer [graphql-schema]]
+   [memefactory.shared.smart-contracts]
+   [mount.core :as mount]
+   [taoensso.timbre :as log]))
 
 (nodejs/enable-util-print!)
 
 (defn -main [& _]
   (-> (mount/with-args
-        {:config {:default {:web3 {:port 8545}}}
-         :smart-contracts {:contracts-var #'memefactory.shared.smart-contracts/smart-contracts}
-         :logging {:level "info"
-                   :console? true}
-         :graphql {:port 6300
-                   :middlewares [logging-middlewares]
-                   :schema (utils/build-schema graphql-schema
-                                               resolvers-map
-                                               {:kw->gql-name graphql-utils/kw->gql-name
-                                                :gql-name->kw graphql-utils/gql-name->kw})
-                   :field-resolver (utils/build-default-field-resolver graphql-utils/gql-name->kw)
-                   :path "/graphql"
-                   :graphiql true}
-         :web3-watcher {:on-online (fn []
-                                     (warn "Ethereum node went online again")
-                                     (mount/stop #'memefactory.server.db/memefactory-db)
-                                     (mount/start #'memefactory.server.db/memefactory-db
-                                                  #'memefactory.server.syncer/syncer
-                                                  #'memefactory.server.emailer/emailer))
-                        :on-offline (fn []
-                                      (warn "Ethereum node went offline")
-                                      (mount/stop #'memefactory.server.syncer/syncer
-                                                  #'memefactory.server.emailer/emailer))}
-         :syncer {:ipfs-config {:host "http://127.0.0.1:5001" :endpoint "/api/v0"}}
-         :ranks-cache {:ttl (t/in-millis (t/minutes 60))}})
+        {:config {:default {:logging {:level :warn
+                                      :sentry {:dsn "https://4bb89c9cdae14444819ff0ac3bcba253@sentry.io/1306960"}}
+                            :graphql {:port 6300
+                                      :middlewares [logging-middlewares]
+                                      :schema (utils/build-schema graphql-schema
+                                                                  resolvers-map
+                                                                  {:kw->gql-name graphql-utils/kw->gql-name
+                                                                   :gql-name->kw graphql-utils/gql-name->kw})
+                                      :field-resolver (utils/build-default-field-resolver graphql-utils/gql-name->kw)
+                                      :path "/graphql"
+                                      :graphiql true}
+                            :web3 {:port 8545}
+                            :ipfs {:host "http://127.0.0.1:5001" :endpoint "/api/v0" :gateway "http://127.0.0.1:8080/ipfs"}
+                            :smart-contracts {:contracts-var #'memefactory.shared.smart-contracts/smart-contracts}
+                            :ranks-cache {:ttl (t/in-millis (t/minutes 60))}
+                            :web3-watcher {:on-online (fn []
+                                                        (log/warn "Ethereum node went online again" ::web3-watcher)
+                                                        (mount/stop #'memefactory.server.db/memefactory-db)
+                                                        (mount/start #'memefactory.server.db/memefactory-db
+                                                                     #'memefactory.server.syncer/syncer
+                                                                     #'memefactory.server.emailer/emailer))
+                                           :on-offline (fn []
+                                                         (log/warn "Ethereum node went offline" ::web3-watcher)
+                                                         (mount/stop #'memefactory.server.syncer/syncer
+                                                                     #'memefactory.server.emailer/emailer))}}}})
       (mount/start))
-  (warn "System started" {:config @config}))
+  (log/warn "System started" {:config @config} ::main))
 
 (set! *main-cli-fn* -main)
