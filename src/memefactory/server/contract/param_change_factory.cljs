f75d3c5db08aebefbb58bd31958ae0e94ae1819b
filip <fbielejec@gmail.com>
2018-12-19 15:49:45 +0100

    cleanup

--- src/memefactory/server/contract/param_change_factory.cljs
+++ src/memefactory/server/contract/param_change_factory.cljs
@@ -1,10 +1,10 @@
 (ns memefactory.server.contract.param-change-factory
   (:require [camel-snake-kebab.core :as cs :include-macros true]
-   [cljs-solidity-sha3.core :refer [solidity-sha3]]
-   [cljs-web3.eth :as web3-eth]
-   [district.server.smart-contracts :refer [contract-call instance contract-address]]
-   [memefactory.server.contract.dank-token :as dank-token]
-   [print.foo :refer [look] :include-macros true]))
+            [cljs-solidity-sha3.core :refer [solidity-sha3]]
+            [cljs-web3.eth :as web3-eth]
+            [district.server.smart-contracts :refer [contract-call instance contract-address]]
+            [memefactory.server.contract.dank-token :as dank-token]
+            [print.foo :refer [look] :include-macros true]))
 
 (defn create-param-change [{:keys [:creator :db :key :value]} & [opts]]
   (contract-call :param-change-factory :create-param-change [creator db (cs/->camelCaseString key) value] (merge {:gas 700000} opts)))
