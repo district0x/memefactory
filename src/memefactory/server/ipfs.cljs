91c511138aa360476ceb4b42c20750a4ea72bf67
fbielejec <fbielejec@gmail.com>
2019-01-18 15:09:52 +0100

    WIP : ipfs error handling

--- src/memefactory/server/ipfs.cljs
+++ src/memefactory/server/ipfs.cljs
@@ -1,14 +1,22 @@
 (ns memefactory.server.ipfs
-  (:require [cljs-ipfs-api.core :as ipfs-core]
-            [district.server.config :refer [config]]
-            [mount.core :as mount :refer [defstate]]))
+  (:require
+   [cljs-ipfs-api.core :as ipfs-core]
+   [cljs-ipfs-api.swarm :as ipfs-swarm]
+   [district.server.config :refer [config]]
+   [mount.core :as mount :refer [defstate]]
+   [taoensso.timbre :as log]
+   ))
 
 (defn start [opts]
-  (try
-    (let [conn (ipfs-core/init-ipfs opts)]      
-      conn)
-    (catch :default e
-      (throw (js/Error. "Can't connect to IPFS node")))))
+  (let [conn (ipfs-core/init-ipfs opts)
+        err-message "Can't connect to IPFS node"]
+    (ipfs-swarm/addrs (fn [err res]
+                        (if-not err
+                          conn
+                          (do (log/error "Can't connect to IPFS node" {:error err
+                                                                       :connection conn}
+                                         ::start)
+                              (throw (js/Error. err-message))))))))
 
 (defstate ipfs
   :start (start (merge (:ipfs @config)
