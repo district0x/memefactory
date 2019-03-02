7d825b78ebbb456db8894f33fa3018589d6c9c56
madvas <matus.lestan@gmail.com>
2019-03-02 11:52:00 +0700

    styling fixes for spinner

--- src/memefactory/styles/component/spinner.clj
+++ src/memefactory/styles/component/spinner.clj
@@ -9,19 +9,22 @@
    [garden.units :refer [px em pt]]))
 
 (defstyles core
-  [:.spinner-outer {:width (px 174)
-                    :height (px 174)
+  [:.spinner-outer {:width (px 104)
+                    :height (px 104)
                     :z-index 5}
-   [:img {:width (px 75)
+   [:img {:width (px 45)
           :height :auto
           :position :relative
           :z-index 10
-          :top (px 64)
-          :left (px 51)}]
+          :top (px 39)
+          :left (px 30)}]
+   [:.spinner-path
+    {:stroke-dasharray 170
+     :stroke-dashoffset 20}]
    [:.spinner-inner
     {:animation "rotate 1.4s linear infinite"
-     :width (px 174)
-     :height (px 174)
-     :top (px -60)
+     :width (px 104)
+     :height (px 104)
+     :top (px -36)
      :position :relative}]]
   (at-keyframes :rotate [:to {:transform "rotate(360deg)"}]))
--- src/memefactory/ui/components/spinner.cljs
+++ src/memefactory/ui/components/spinner.cljs
@@ -3,22 +3,21 @@
 (defn spin []
   [:div.spinner-outer
    [:img {:src "/assets/icons/mememouth.png"}]
-   [:svg.spinner-inner {:width "174px"
-                        :height "174px"
-                        :viewBox"0 0 66 66"}
+   [:svg.spinner-inner {:viewBox"0 0 66 66"}
     [:circle {:fill "#FFFF"
               :cx "33"
               :cy "33"
               :r "30"
               :stroke-width "2"
-              :stroke "url(#gradient)"}]
+              :stroke "url(#gradient)"
+              :class "spinner-path"}]
     [:linearGradient {:id "gradient"}
      [:stop {:offset "50%"
-             :stop-color "#808080"
+             :stop-color "#e7ebf8"
              :stop-opacity "1"}]
      [:stop {:offset "65%"
-             :stop-color "#808080"
+             :stop-color "#e7ebf8"
              :stop-opacity ".5"}]
      [:stop {:offset "100%"
-             :stop-color "#808080"
+             :stop-color "#e7ebf8"
              :stop-opacity "0"}]]]])
