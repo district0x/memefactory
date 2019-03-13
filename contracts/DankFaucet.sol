aa98e74697aa3443265ae616dc310b10c9e3301a
Daniel Glauser <daniel@cambium.consulting>
2019-03-13 11:55:38 -0600

    The OAR should remain commented out in qa and production

--- contracts/DankFaucet.sol
+++ contracts/DankFaucet.sol
@@ -47,7 +47,7 @@ contract DankFaucet is usingOraclize {
 
     // Comment out this line when deploying to production
     // See: https://github.com/oraclize/ethereum-bridge
-    OAR = OraclizeAddrResolverI(0x6f485C8BF6fc43eA212E93BBF8ce046C7f1cb475);
+    //OAR = OraclizeAddrResolverI(0x6f485C8BF6fc43eA212E93BBF8ce046C7f1cb475);
   }
 
   /**
