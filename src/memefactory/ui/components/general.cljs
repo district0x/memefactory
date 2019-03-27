4bf7241effaab54295416c965f3cf4ef5d259c35
Benjamin Zaporzan <ben@district0x.io>
2019-03-26 23:44:07 -0400

    Fixes to submission button styling - fixes issue #286 - fixes issue #285

--- src/memefactory/ui/components/general.cljs
+++ src/memefactory/ui/components/general.cljs
@@ -1,6 +1,6 @@
 (ns memefactory.ui.components.general)
 
-(defn dank-with-logo [ammount]
+(defn dank-with-logo [amount]
   [:div.dank-wrapper
-   [:span ammount]
+   [:span amount]
    [:img {:src "/assets/icons/dank-logo.svg"}]])
