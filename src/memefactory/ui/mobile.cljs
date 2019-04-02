4bed17acf12840ac7eb337446d871fa52d6eece3
Benjamin Zaporzan <ben@district0x.io>
2019-04-02 00:03:08 -0400

    Added missing file for mobile fixes

---
+++ src/memefactory/ui/mobile.cljs
@@ -0,0 +1,7 @@
+(ns memefactory.ui.mobile)
+
+
+(def coinbase-appstore-links
+  {:main-mobile-link "https://wallet.coinbase.com/"
+   :android-mobile-link "https://play.google.com/store/apps/details?id=org.toshi"
+   :ios-mobile-link "https://itunes.apple.com/app/coinbase-wallet/id1278383455?ls=1&mt=8"})
