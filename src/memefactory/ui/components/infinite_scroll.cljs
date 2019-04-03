f0629523e7cf6c77eb3fa27140ed4d3f59c00883
fbielejec <fbielejec@gmail.com>
2019-04-03 13:27:54 +0200

    WIP : scrolling controll

--- src/memefactory/ui/components/infinite_scroll.cljs
+++ src/memefactory/ui/components/infinite_scroll.cljs
@@ -7,26 +7,6 @@
 
 (def react-infinite (r/adapt-react-class js/Infinite))
 
-;; (def card-height 400) ;; px
-;; (def default-onload-threshold (* 1.5 card-height)) ;; px
-;; (def default-debounce-interval 250) ;; ms
-
-
-(defn- get-scroll-top []
-  (if (exists? (.-pageYOffset js/window))
-    (.-pageYOffset js/window)
-    (.-scrollTop (or (.-documentElement js/document)
-                     (.-parentNode (.-body js/document))
-                     (.-body js/document)))))
-
-(defn- get-el-top-position [node]
-  (if (not node)
-    0
-    (+ (.-offsetTop node) (get-el-top-position (.-offsetParent node)))))
-
-(defn- safe-component-mounted? [component]
-  (try (boolean (r/dom-node component)) (catch js/Object _ false)))
-
 (defn- get-page-height []
   (let [body (.-body js/document)
         html (.-documentElement js/document)]
@@ -36,71 +16,9 @@
          (.-scrollHeight html)
          (.-offsetHeight html))))
 
-(defn- element-scroll-bounds [el-node]
-  (let [bounds (.getBoundingClientRect el-node)]
-    {:height (aget bounds "height")
-     :top (aget bounds "top")
-     :bottom (aget bounds "bottom")
-     :page-height (get-page-height)
-     :page-bottom (+ (.-innerHeight js/window) (.-scrollY js/window))}))
-
-(defn- load-more? [el-node onload-threshold]
-  (let [{:keys [page-height page-bottom]} (element-scroll-bounds el-node)
-        bottom-offset (- page-height page-bottom)]
-    (<= bottom-offset onload-threshold)))
-
-#_(defn infinite-scroll [props]
-  (let [listener-fn (atom nil)
-        detach-scroll-listener (fn []
-                                 (when @listener-fn
-                                   (.removeEventListener js/window "scroll" @listener-fn)
-                                   (.removeEventListener js/window "resize" @listener-fn)
-                                   (reset! listener-fn nil)))
-        should-load-more? (fn [this]
-                            (let [node (r/dom-node this)
-                                  ;; scroll-top (get-scroll-top)
-                                  ;; my-top (get-el-top-position node)
-                                  ;; threshold 50
-                                  ]
-
-                              (load-more? node default-onload-threshold)
-
-                              ;; (log/debug "load-more?" {:load? (> threshold (- (.-offsetHeight node)
-                              ;;                                                 (+ (.-innerHeight js/window)
-                              ;;                                                    scroll-top)))
-                              ;;                          :t threshold
-                              ;;                          :v (- (.-offsetHeight node)
-                              ;;                                (+ (.-innerHeight js/window)
-                              ;;                                   scroll-top))})
-
-                              ;; (> threshold
-                              ;;    (- (.-offsetHeight node)
-                              ;;       (+ (.-innerHeight js/window)
-                              ;;          scroll-top)))
-
-                              ))
-        scroll-listener (fn [this]
-                          (when (safe-component-mounted? this)
-                            (let [{:keys [load-fn]} (r/props this)]
-                              (when (should-load-more? this)
-                                (detach-scroll-listener)
-                                (load-fn)))))
-        debounced-scroll-listener (utils/debounce scroll-listener default-debounce-interval)
-        attach-scroll-listener (fn [this]
-                                 (when-not @listener-fn
-                                   (reset! listener-fn (partial debounced-scroll-listener this))
-                                   (.addEventListener js/window "scroll" @listener-fn)
-                                   (.addEventListener js/window "resize" @listener-fn)))]
-    (r/create-class
-     {:component-did-mount attach-scroll-listener
-      :component-did-update attach-scroll-listener
-      :component-will-unmount detach-scroll-listener
-      :reagent-render (fn [] [:div.infinite-scroll])})))
-
-
-
 (defn infinite-scroll [{:keys [:class :element-height :use-window-as-scroll-container
                                :container-height :infinite-load-begin-edge-offset
+                               :loading?
                                :loading-spinner-delegate
                                :load-fn]
                         :or {class :infinite-scroll
@@ -112,20 +30,21 @@
                                                         [:div.loading-spinner-delegate "Loading..." ])}
                         :as props} & [children]]
   (let [load-disabled? (r/atom false)
-        loading? (r/atom false)]
-    [react-infinite (spy (r/merge-props (dissoc props :load-fn)
-                                        {:class class
-                                         :element-height element-height
-                                         :use-window-as-scroll-container use-window-as-scroll-container
-                                         :container-height container-height
-                                         :infinite-load-begin-edge-offset infinite-load-begin-edge-offset
-                                         :loading-spinner-delegate loading-spinner-delegate
-                                         :is-infinite-loading @loading?
-                                         :on-infinite-load (fn []
-                                                             (when (and (not @load-disabled?)
-                                                                        (not @loading?))
-                                                               (reset! load-disabled? true)
-                                                               (js/setTimeout #(reset! load-disabled? false) 300)
-                                                               (load-fn)
-                                                               (reset! load-disabled? false)))}))
+        ;; loading? (r/atom false)
+        ]
+    [react-infinite (r/merge-props (dissoc props :load-fn :is-infinite-loading)
+                                   {:class class
+                                    :element-height element-height
+                                    :use-window-as-scroll-container use-window-as-scroll-container
+                                    :container-height container-height
+                                    :infinite-load-begin-edge-offset infinite-load-begin-edge-offset
+                                    :loading-spinner-delegate loading-spinner-delegate
+                                    :is-infinite-loading (spy loading?)
+                                    :on-infinite-load (fn []
+                                                        (when (and (not @load-disabled?)
+                                                                   (not loading?))
+                                                          (reset! load-disabled? true)
+                                                          (js/setTimeout #(reset! load-disabled? false) 300)
+                                                          (load-fn)
+                                                          (reset! load-disabled? false)))})
      children]))
--- src/memefactory/ui/dank_registry/browse_page.cljs
+++ src/memefactory/ui/dank_registry/browse_page.cljs
@@ -1,5 +1,9 @@
 (ns memefactory.ui.dank-registry.browse-page
   (:require
+   [ajax.core :as ajax :refer [POST]]
+   [district.ui.graphql.utils :as graphql-ui-utils]
+   [district.graphql-utils :as graphql-utils]
+
    [district.ui.component.page :refer [page]]
    [district.ui.graphql.subs :as gql]
    [district.ui.router.subs :as router-subs]
@@ -10,15 +14,12 @@
    [memefactory.ui.components.tiles :as tiles]
    [memefactory.ui.dank-registry.events :as mk-events]
    [memefactory.ui.components.spinner :as spinner]
-   [print.foo :refer [look] :include-macros true]
    [re-frame.core :refer [subscribe dispatch]]
    [reagent.core :as r]
-   [taoensso.timbre :as log]
+   [taoensso.timbre :as log :refer [spy]]
    [memefactory.ui.components.panels :refer [no-items-found]]))
 
 (def page-size 3 #_12)
-
-;; (def state (atom []))
 
 (defn build-tiles-query [{:keys [:search-term :order-by :search-tags :order-dir :only-cheapest?]} after]
   [:search-memes
@@ -51,7 +52,7 @@
 
 ;;;;;;;;;;;;;;;;;;;;;;
 
-(def scroll-interval 3)
+;; (def scroll-interval 3)
 
 (def elem-height 400)
 
@@ -68,44 +69,98 @@
                     :border-bottom "1px solid #000"}}
       (str "ELEM " i)])))
 
-(defn infinite-list []
-  (let [state (r/atom nil)
+(defn puke [col]
+  [:pre {:style {:height (str elem-height "px")
+                 :text-align :center
+                 :width "120px"
+                 :margin :auto
+                 :background-color "#ddd"
+                 :border-bottom "1px solid #000"}}
+   (with-out-str (cljs.pprint/pprint col))])
+
+;;;;;;;;;;;;;;;;;;;;;;
+
+(defn dank-registry-tiles [form-data meme-search]
+  (let [
+
+        ;; all-memes (->> (spy @meme-search)
+        ;;                (mapcat (fn [r] (-> r :search-memes :items))))
+
+        ;; last-meme (last @meme-search)
+
+        state (r/atom nil)
+        loading? (r/atom false)
+
         update-state (fn [{:keys [:from :to]}]
-                       (log/debug "@update-state" {:f from :t to})
-                       (swap! state (fn [old new] (into old (concat new)))
+
+                       (log/debug "@update-state" {
+                                                   ;; :q (graphql-ui-utils/parse-query {:queries [(build-tiles-query @form-data to)]}
+                                                   ;;                                  {:kw->gql-name graphql-utils/kw->gql-name})
+                                                   :f from
+                                                   :t to})
+
+                       #_(swap! state (fn [old new] (into old (concat new)))
                               (build-elems from to))
-                       )]
+
+                       (reset! loading? true)
+
+                       (POST "http://qa.district0x.io:6300/graphql"
+                           :params {:query (:query-str (graphql-ui-utils/parse-query {:queries [(build-tiles-query @form-data (str to))]}
+                                                                                     {:kw->gql-name graphql-utils/kw->gql-name}))
+                                    :id @form-data}
+                           :format :json
+                           :response-format :json
+                           :handler #_#(log/info "response: "(graphql-utils/js->clj-response (clj->js %) {:gql-name->kw graphql-utils/gql-name->kw}))
+                           #(do
+                              (swap! state (fn [old new] (into old (concat new)))
+                                       (get-in (graphql-utils/js->clj-response (clj->js %) {:gql-name->kw graphql-utils/gql-name->kw})
+                                               [:data :search-memes :items]))
+                              (reset! loading? false))
+                           :error-handler #(log/error "error: " %))
+
+
+                       )
+
+        ]
 
     (fn []
 
       (log/debug "#state" {:c (-> @state count)})
 
-      [infinite-scroll/infinite-scroll {:load-fn (fn []
+      (if true #_(empty? all-memes)
+          ;;[no-items-found]
+          [:div.scroll-area
+           [:div.tiles
+            [infinite-scroll/infinite-scroll {:loading? @loading?
+                                              :load-fn (fn []
 
-                                                   (let [from (-> @state count)
-                                                         to (+ scroll-interval from)]
+                                                         (log/debug "@on-load")
 
-                                                     (log/debug "@on-load" {:f from :t to})
+                                                         #_(let [{:keys [:has-next-page :end-cursor]} (:search-memes last-meme)]
+                                                             (when has-next-page
+                                                               (dispatch [:district.ui.graphql.events/query
+                                                                          {:query {:queries [(build-tiles-query @form-data end-cursor)]}
+                                                                           :id @form-data}])))
 
-                                                     (update-state {:from from :to to}))
+                                                         (let [from (-> @state count)
+                                                               to (+ page-size from)]
+                                                           (log/debug "@on-load" {:f from :t to})
+                                                           (update-state {:from from :to to}))
 
-                                                   )}
-       (reverse @state)])
+                                                         )}
 
-    ))
+             #_(reverse @state)
 
-;;;;;;;;;;;;;;;;;;;;;;
+             (doall
+              (for [{:keys [:reg-entry/address] :as meme} (reverse @state)]
+                ^{:key address} [puke meme]))
 
-(defn dank-registry-tiles [form-data meme-search]
-  (let [all-memes (->> @meme-search
-                       (mapcat (fn [r] (-> r :search-memes :items))))
-        last-meme (last @meme-search)
+             ;; (doall
+             ;;  (for [{:keys [:reg-entry/address] :as meme} all-memes]
+             ;;    ^{:key address} [tiles/meme-tile meme]))
 
+             ]]]))
 
-
-        ]
-
-    [infinite-list]
 
     #_[:div.scroll-area
        [:div.tiles
@@ -130,38 +185,36 @@
 
 
 (defmethod page :route.dank-registry/browse []
-
-  ;; [infinite-list]
-
   (let [active-page (subscribe [::router-subs/active-page])
         form-data (let [{:keys [query]} @active-page]
                     (r/atom {:term ""
                              :order-by (or (:order-by query) "number-desc")
                              :order-dir (or (:order-dir query) "desc")}))
         all-tags-subs (subscribe [::gql/query {:queries [[:search-tags [[:items [:tag/name]]]]]}])]
-    (let [re-search (fn [& _]
-                      (dispatch [:district.ui.graphql.events/query
-                                 {:query {:queries [(build-tiles-query @form-data nil)]}}]))
-          meme-search (subscribe [::gql/query {:queries [(build-tiles-query @form-data nil)]}
-                                  {:id @form-data
-                                   :disable-fetch? false}])
-          search-total-count (-> @meme-search first :search-memes :total-count)]
-      [app-layout
-       {:meta {:title "MemeFactory - Dank Registry"
-               :description "Browse all memes ever minted. MemeFactory is decentralized registry and marketplace for the creation, exchange, and collection of provably rare digital assets."}}
-       [:div.dank-registry-index-page
-        [search-tools {:form-data form-data
-                       :tags (->> @all-tags-subs :search-tags :items (mapv :tag/name))
-                       :search-id :search-term
-                       :selected-tags-id :search-tags
-                       :title "Dank registry"
-                       :sub-title "Browse all memes ever minted"
-                       :on-selected-tags-change re-search
-                       ;; TODO: Fix this hack, we need a way of passing a select more info
-                       :select-options (->> [{:key "number-desc"             :value "Newest"}
-                                             {:key "total-trade-volume" :value "Total trade volume"}
-                                             {:key "number-asc"             :value "Registry Number"}])
-                       :on-search-change re-search
-                       :on-check-filter-change re-search
-                       :on-select-change re-search}]
-        [dank-registry-tiles form-data meme-search]]])))
+    (fn []
+      (let [re-search (fn [& _]
+                        (dispatch [:district.ui.graphql.events/query
+                                   {:query {:queries [(build-tiles-query @form-data nil)]}}]))
+            meme-search (subscribe [::gql/query {:queries [(build-tiles-query @form-data nil)]}
+                                    {:id @form-data
+                                     :disable-fetch? false}])
+            search-total-count (-> @meme-search first :search-memes :total-count)]
+        [app-layout
+         {:meta {:title "MemeFactory - Dank Registry"
+                 :description "Browse all memes ever minted. MemeFactory is decentralized registry and marketplace for the creation, exchange, and collection of provably rare digital assets."}}
+         [:div.dank-registry-index-page
+          [search-tools {:form-data form-data
+                         :tags (->> @all-tags-subs :search-tags :items (mapv :tag/name))
+                         :search-id :search-term
+                         :selected-tags-id :search-tags
+                         :title "Dank registry"
+                         :sub-title "Browse all memes ever minted"
+                         :on-selected-tags-change re-search
+                         ;; TODO: Fix this hack, we need a way of passing a select more info
+                         :select-options (->> [{:key "number-desc"             :value "Newest"}
+                                               {:key "total-trade-volume" :value "Total trade volume"}
+                                               {:key "number-asc"             :value "Registry Number"}])
+                         :on-search-change re-search
+                         :on-check-filter-change re-search
+                         :on-select-change re-search}]
+          [dank-registry-tiles form-data meme-search]]]))))
