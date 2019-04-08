597fda1b7cbb961fefe4632d5f03af68b4885939
fbielejec <fbielejec@gmail.com>
2019-04-08 16:51:28 +0200

    WIP : debugging pageheight shrinking

--- src/memefactory/styles/app_menu.clj
+++ src/memefactory/styles/app_menu.clj
@@ -25,134 +25,134 @@
    :width (px (/ tracker-width 2))})
 
 (defstyles core
-  [:.app-container
-   [:.app-menu
-    {:transition "left 0.3s"}
+  ;;:.app-container
+  [:.app-menu
+   {:transition "left 0.3s"}
+   (for-media-max :tablet
+                  [:&
+                   {:z-index 10
+                    :display :block
+                    :left 0
+                    :width (px 270)
+                    :background-color (color :white)
+                    :min-height (px 1800)
+                    :position :absolute}])
+   [:&.closed
     (for-media-max :tablet
                    [:&
-                    {:z-index 10
-                     :display :block
-                     :left 0
-                     :width (px 270)
-                     :background-color (color :white)
-                     :min-height (px 1800)
-                     :position :absolute}])
-    [:&.closed
+                    {:left (px -270)}])]
+   {:position "relative"}
+   [(sel/> :.menu-content :.node :.node-content :.item)
+    (border-top {:color (color :border-line)})
+    {:padding-top (em 1)
+     :padding-bottom (em 1)}]
+   [:.menu-content
+    [:.mf-logo
+     {:cursor :pointer
+      :padding-top (em 3)
+      :padding-bottom (em 1)
+      :padding-left (em 1.7)
+      :padding-right (em 1.7)
+      :display :grid
+      :grid-template-columns "50% 50%"}
      (for-media-max :tablet
                     [:&
-                     {:left (px -270)}])]
-    {:position "relative"}
-    [(sel/> :.menu-content :.node :.node-content :.item)
-     (border-top {:color (color :border-line)})
-     {:padding-top (em 1)
-      :padding-bottom (em 1)}]
-    [:.menu-content
-     [:.mf-logo
-      {:cursor :pointer
-       :padding-top (em 3)
-       :padding-bottom (em 1)
-       :padding-left (em 1.7)
-       :padding-right (em 1.7)
-       :display :grid
-       :grid-template-columns "50% 50%"}
-      (for-media-max :tablet
-                     [:&
-                      {:grid-template-columns "30% 50%"}])
-      [:img
-       {:width (em 4)}]
-      [:span
-       (font :bungee)
-       {:font-size (px 18)
-        :line-height (em 1.4)
-        :color (color :menu-logo)}]]
+                     {:grid-template-columns "30% 50%"}])
+     [:img
+      {:width (em 4)}]
+     [:span
+      (font :bungee)
+      {:font-size (px 18)
+       :line-height (em 1.4)
+       :color (color :menu-logo)}]]
 
-     [:.accounts
-      {:display :none}
-      (for-media-max
-       :tablet
-       [:& {:display :grid}])]
+    [:.accounts
+     {:display :none}
+     (for-media-max
+      :tablet
+      [:& {:display :grid}])]
 
-     [:ul.node
-      {:padding-left (em 0)
-       :margin 0}
-      [:.item.active
-       [:a {:color (color :pink)
-            :cursor :pointer}]]
-      [:.item.disabled
-       [:a {:opacity 0.5}]]]
+    [:ul.node
+     {:padding-left (em 0)
+      :margin 0}
+     [:.item.active
+      [:a {:color (color :pink)
+           :cursor :pointer}]]
+     [:.item.disabled
+      [:a {:opacity 0.5}]]]
 
-     [:ul.node
-      [:.item {:font-size (pt 12)}]
-      [:ul.node {:padding-left (em 0)}
-       [:.item {:font-size (pt 10)}]]]
+    [:ul.node
+     [:.item {:font-size (pt 12)}]
+     [:ul.node {:padding-left (em 0)}
+      [:.item {:font-size (pt 10)}]]]
 
-     [:ul.node
-      {:list-style :none}
-      [:.item
-       {:display :flex
-        :align-items :center
-        :padding-top menu-gutter
-        :padding-bottom menu-gutter}
-       [:a
-        {:color (color :menu-text)
-         :margin-left (rem 4)
-         :cursor :pointer}
-        [:&:hover
-         {:color (color :pink)}]
-        [:&:before
-         {:display :block
-          :background-size [(rem 1.8) (rem 1.8)]
-          :background-repeat :no-repeat
-          :margin-left (rem -3)
-          :margin-top (rem -0.2)
-          :position :absolute
-          :height (rem 1.8)
-          :width (rem 1.8)}]]
+    [:ul.node
+     {:list-style :none}
+     [:.item
+      {:display :flex
+       :align-items :center
+       :padding-top menu-gutter
+       :padding-bottom menu-gutter}
+      [:a
+       {:color (color :menu-text)
+        :margin-left (rem 4)
+        :cursor :pointer}
+       [:&:hover
+        {:color (color :pink)}]
+       [:&:before
+        {:display :block
+         :background-size [(rem 1.8) (rem 1.8)]
+         :background-repeat :no-repeat
+         :margin-left (rem -3)
+         :margin-top (rem -0.2)
+         :position :absolute
+         :height (rem 1.8)
+         :width (rem 1.8)}]]
 
-       (let [icons [[:dankregistry "dankregistry"]
-                    [:about "about"]
-                    [:marketplace "marketplace"]
-                    [:how-it-works "how-it-works"]
-                    [:leaderboard "leaderboard"]
-                    [:memefolio "mymemefolio"]
-                    [:my-settings "mysettings"]
-                    [:faucet "dank-logo"]]]
-         (mapv (fn [[cls img]]
-                 [(keyword (str "&." (name cls)))
-                  [:a:before
-                   {:content "''"
-                    :background-image (str "url('/assets/icons/" img ".svg')")}]])
-               icons))]
-      [:.item.faucet
-       [:a:before {:background-size "3.1rem"
-                   :background-position (px -7)
-                   :height (rem 2.1)
-                   :width (rem 2.2)
-                   :margin-top (px -6)}]]]]
-    [:.district0x-banner
-     (font :filson)
-     {:padding-left (em 3)
-      :color (color :menu-text)
-      ;; :font-weight :bold
-      :line-height (em 1.8)
-      :font-size (pt 8)
-      :position :absolute
-      :overflow :visible
-      :display :block
-      :height (em 8)
-      :right 0
-      :left 0
-      :padding-bottom (em 20)
-      :bottom (em 0)
-      :background-size ["100%"  "100%"]
-      :background-position "right bottom";
-      :background-repeat :no-repeat
-      :background-image "url('/assets/icons/conveyer.png')"}
-     [:a {:text-decoration :underline}]
-     [:.logo {:content "url('/assets/icons/district0x.svg')"
-              :height (em 2)
-              ;; :background-position "left bottom";
-              ;; :background-repeat :no-repeat
-              ;; :background-image ""
-              :margin-top (em -5)
-              :margin-bottom (em 1)}]]]])
+      (let [icons [[:dankregistry "dankregistry"]
+                   [:about "about"]
+                   [:marketplace "marketplace"]
+                   [:how-it-works "how-it-works"]
+                   [:leaderboard "leaderboard"]
+                   [:memefolio "mymemefolio"]
+                   [:my-settings "mysettings"]
+                   [:faucet "dank-logo"]]]
+        (mapv (fn [[cls img]]
+                [(keyword (str "&." (name cls)))
+                 [:a:before
+                  {:content "''"
+                   :background-image (str "url('/assets/icons/" img ".svg')")}]])
+              icons))]
+     [:.item.faucet
+      [:a:before {:background-size "3.1rem"
+                  :background-position (px -7)
+                  :height (rem 2.1)
+                  :width (rem 2.2)
+                  :margin-top (px -6)}]]]]
+   [:.district0x-banner
+    (font :filson)
+    {:padding-left (em 3)
+     :color (color :menu-text)
+     ;; :font-weight :bold
+     :line-height (em 1.8)
+     :font-size (pt 8)
+     :position :absolute
+     :overflow :visible
+     :display :block
+     :height (em 8)
+     :right 0
+     :left 0
+     :padding-bottom (em 20)
+     :bottom (em 0)
+     :background-size ["100%"  "100%"]
+     :background-position "right bottom";
+     :background-repeat :no-repeat
+     :background-image "url('/assets/icons/conveyer.png')"}
+    [:a {:text-decoration :underline}]
+    [:.logo {:content "url('/assets/icons/district0x.svg')"
+             :height (em 2)
+             ;; :background-position "left bottom";
+             ;; :background-repeat :no-repeat
+             ;; :background-image ""
+             :margin-top (em -5)
+             :margin-bottom (em 1)}]]])
--- src/memefactory/ui/components/infinite_scroll.cljs
+++ src/memefactory/ui/components/infinite_scroll.cljs
@@ -6,8 +6,11 @@
 (defn- safe-component-mounted? [component]
   (try (boolean (spy (r/dom-node component))) (catch js/Object _ false)))
 
+#_(defn prevent-default [evt]
+  (.preventDefault evt)
+  (goog.object/set evt "returnValue" false))
+
 (defn infinite-scroll [props]
-  #_[:div "FUBAR"]
   (let [root-id "app-container"
         state (atom {:page-height 0
                      :listener-fn nil})
@@ -22,29 +25,33 @@
                           :window-height window-height
                           :scroll-position scroll-position
                           :bottom? (<= page-height (+ window-height scroll-position))
-                          :to-bottom (- page-height (+ window-height scroll-position))})
-                       )
+                          :to-bottom (- page-height (+ window-height scroll-position))}))
         should-load-more? (fn [this]
                             (let [{:keys [:infinite-load-threshold]
                                    :or {infinite-load-threshold 50}} (r/props this)
                                   {:keys [:bottom? :to-bottom] :as p} (get-position root-id)]
-                              (log/debug (str "should load more? " (<= to-bottom infinite-load-threshold)) p)
-                              ;; bottom?
-                              (<= to-bottom infinite-load-threshold)
-                              ))
-        scroll-listener (fn [this]
-                          ;;when (safe-component-mounted? this)
-                          (let [{:keys [:load-fn :has-more? :loading?] :as p} (r/props this)]
-                            (log/debug "scrolled to load more" (select-keys p [:has-more? :loading?]))
-                            (when (and has-more?
-                                       (not loading?)
-                                       (should-load-more? this))
+                              (<= to-bottom infinite-load-threshold)))
+        scroll-listener (fn [this evt]
+                          (let [{:keys [:load-fn :has-more? :loading?] :as p} (r/props this)
+                                load-more? (and has-more?
+                                                (not loading?)
+                                                (should-load-more? this))]
+
+                            (log/debug "scroll event" (select-keys (merge {:load-more? load-more?}
+                                                                          (get-position root-id))
+                                                                   [:page-height
+                                                                    :previous-page-height
+                                                                    :to-bottom
+                                                                    :scroll-position
+                                                                    :window-height]))
+
+                            (when load-more?
                               (update-state! (select-keys (get-position root-id) [:page-height]))
                               ;; (detach-scroll-listener)
                               (load-fn))))
         attach-scroll-listener (fn [this]
                                  (let [{:keys [:has-more? :debounce-interval]
-                                        :or {:debounce-interval 200}} (r/props this)
+                                        :or {:debounce-interval 0 #_200}} (r/props this)
                                        {:keys [:listener-fn]} @state
                                        debounced-scroll-listener (utils/debounce scroll-listener debounce-interval)]
                                    (when has-more?
@@ -62,17 +69,28 @@
      {:component-did-mount attach-scroll-listener
       :component-did-update (fn [this _]
                               (do (attach-scroll-listener this)
-                                (let [{:keys [:load-fn :has-more? :loading?]} (r/props this)
-                                      {:keys [:bottom? :page-height]} (get-position root-id)
-                                      {previous-page-height :page-height} @state]
-                                  (when (and has-more?
-                                             bottom?)
+                                  (let [{:keys [:load-fn :has-more? :loading?]} (r/props this)
+                                        {:keys [:bottom? :page-height]} (get-position root-id)
+                                        {previous-page-height :page-height} @state]
 
-                                    (log/debug "stuck at bottom, autoscrolling!" {:page-height page-height
-                                                                                  :previous-page-height previous-page-height})
+                                    (when (and has-more?
+                                               bottom?)
 
-                                    (update-state! {:page-height page-height})
-                                    (-> js/window (.scrollBy 0 (- previous-page-height page-height)))))))
+                                      (log/debug "stuck at bottom, autoscrolling!" (select-keys (merge (get-position root-id)
+                                                                                                       {:page-height page-height
+                                                                                                        :previous-page-height previous-page-height})
+                                                                                                [:page-height
+                                                                                                 :previous-page-height
+                                                                                                 :to-bottom
+                                                                                                 :scroll-position
+                                                                                                 :window-height]))
+
+
+
+                                      (-> js/window (.scrollBy 0 (spy (- previous-page-height page-height))))
+                                      (update-state! {:page-height page-height})
+
+                                      ))))
       :component-will-unmount detach-scroll-listener
       :reagent-render (fn [props]
                         [:div.infinite-scroll])})))
