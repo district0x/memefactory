ffbf0c248b32ceeaea6f2c80d13372429af17125
fbielejec <fbielejec@gmail.com>
2019-04-15 17:32:32 +0200

    gzipped content, minimized container images

--- docker-builds/ui/memefactory.io
+++ docker-builds/ui/memefactory.io
@@ -23,12 +23,12 @@ server {
     try_files $uri $uri/index.html /index.html;
   }
 
-   location = /X0X.html {
-     root /usr/share/nginx/html/;
-     internal;
+  location = /X0X.html {
+    root /usr/share/nginx/html/;
+    internal;
    }
 
-    # redirect server error pages to the static error page
+  # redirect server error pages to the static error page
   error_page 400 401 402 403 404 405 406 407 408 409 410 411 412 413 414 415 416 417 418 420 422 423 424 426 428 429 431 444 449 450 451 500 501 502 503 504 505 506 507 508 509 510 511 /X0X.html;
 
 }
--- docker-push.sh
+++ docker-push.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+#--- FUNCTIONS
+
 function build {
   NAME=$1
   TAG=$(git log -1 --pretty=%h)
@@ -10,6 +12,20 @@ function build {
   echo "=============================="
   echo  "["$SERVICE"]" "Buidling: " $IMG
   echo "=============================="
+
+  case $SERVICE in
+    "ui")
+      lein garden once
+      env MEMEFACTORY_ENV=qa lein cljsbuild once "ui"
+      ;;
+    "server")
+      lein cljsbuild once "server"
+      ;;
+    *)
+      echo "ERROR: don't know what to do with SERVICE: "$SERVICE""
+      exit 1
+      ;;
+  esac
 
   docker build -t $IMG -f docker-builds/$SERVICE/Dockerfile .
   docker tag $IMG $LATEST
@@ -25,10 +41,21 @@ function login {
   echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
 }
 
+function before {
+  lein deps
+  lein npm install
+  lein solc once
+}
+
+#--- EXECUTE
+
+before
 login
 
-images=(district0x/memefactory-server
-        district0x/memefactory-ui)
+images=(
+  district0x/memefactory-server
+  district0x/memefactory-ui
+)
 
 for i in "${images[@]}"; do
   (
