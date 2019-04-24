5efd3cf1adeb08f7a289ad3d4ff3ab6a3a93d168
madvas <matus.lestan@gmail.com>
2019-04-24 13:23:26 +0700

    Start using district-ui-ipfs

--- project.clj
+++ project.clj
@@ -43,6 +43,7 @@
                  [district0x/district-ui-component-notification "1.0.0"]
                  [district0x/district-ui-component-tx-button "1.0.0"]
                  [district0x/district-ui-graphql "1.0.9"]
+                 [district0x/district-ui-ipfs "1.0.0"]
                  [district0x/district-ui-logging "1.0.4"]
                  [district0x/district-ui-mobile "1.0.0"]
                  [district0x/district-ui-notification "1.0.1"]
