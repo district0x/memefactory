6b5052cf86aeb551b056d08f52edf1c56dd3bc4f
Filip Bielejec <fbielejec@gmail.com>
2019-04-26 16:22:14 +0200

    Feature/env builds (#416)
    * split builds by branch and ENV_VAR (set by CI)

--- docker-push.sh
+++ docker-push.sh
@@ -1,26 +1,31 @@
 #!/bin/bash
 
+#--- ARGS
+
+BUILD_ENV=$1
+
 #--- FUNCTIONS
 
 function build {
   {
     NAME=$1
+    BUILD_ENV=$2
     TAG=$(git log -1 --pretty=%h)
     IMG=$NAME:$TAG
-    LATEST=$NAME:latest
+
     SERVICE=$(echo $NAME | cut -d "-" -f 2)
 
-    echo "=============================="
-    echo  "["$SERVICE"] Buidling: "$IMG""
-    echo "=============================="
+    echo "============================================="
+    echo  "["$BUILD_ENV"] ["$SERVICE"] Buidling: "$IMG""
+    echo "============================================="
 
     case $SERVICE in
       "ui")
         lein garden once
-        env MEMEFACTORY_ENV=qa lein cljsbuild once "ui"
+        env MEMEFACTORY_ENV=$BUILD_ENV lein cljsbuild once "ui"
         ;;
       "server")
-        lein cljsbuild once "server"
+        env MEMEFACTORY_ENV=$BUILD_ENV lein cljsbuild once "server"
         ;;
       *)
         echo "ERROR: don't know what to do with SERVICE: "$SERVICE""
@@ -29,7 +34,22 @@ function build {
     esac
 
     docker build -t $IMG -f docker-builds/$SERVICE/Dockerfile .
-    docker tag $IMG $LATEST
+
+    case $BUILD_ENV in
+      "qa")
+        # qa images are tagged as `latest`
+        docker tag $IMG $NAME:latest
+        ;;
+      "production")
+        # prod images are tagged as `release`
+        docker tag $IMG $NAME:release
+        ;;
+      *)
+        echo "ERROR: don't know what to do with BUILD_ENV: "$BUILD_ENV""
+        exit 1
+        ;;
+    esac
+
   } || {
     echo "EXCEPTION WHEN BUIDLING "$IMG""
     exit 1
@@ -64,7 +84,7 @@ images=(
 
 for i in "${images[@]}"; do
   (
-    build $i
+    build $i $BUILD_ENV
     push $i
   )
 
