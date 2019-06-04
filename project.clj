51f3714140e41a50ada7f95e2da8f801cc4e4069
Juan Monetta <jpmonettas@gmail.com>
2019-06-04 08:46:12 -0300

    Fix #495 (Putting double quote " into search breaks a page)

--- project.clj
+++ project.clj
@@ -29,7 +29,7 @@
                  [district0x/district-sendgrid "1.0.0"]
                  [district0x/district-server-config "1.0.1"]
                  [district0x/district-server-db "1.0.3"]
-                 [district0x/district-server-graphql "1.0.15"]
+                 [district0x/district-server-graphql "1.0.16"]
                  [district0x/district-server-logging "1.0.5"]
                  [district0x/district-server-middleware-logging "1.0.0"]
                  [district0x/district-server-smart-contracts "1.0.14"]
