b02113d6996258ab1e2fff04f5be37536b808185
filip <fbielejec@gmail.com>
2019-07-18 14:44:04 +0200

    closes #553

--- project.clj
+++ project.clj
@@ -47,7 +47,7 @@
                  [district0x/district-ui-component-tx-button "1.0.0"]
                  [district0x/district-ui-graphql "1.0.9"]
                  [district0x/district-ui-ipfs "1.0.0"]
-                 [district0x/district-ui-logging "1.0.4"]
+                 [district0x/district-ui-logging "1.1.0"]
                  [district0x/district-ui-mobile "1.0.0"]
                  [district0x/district-ui-notification "1.0.1"]
                  [district0x/district-ui-now "1.0.2"]
