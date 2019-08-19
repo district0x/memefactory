445cddb5033bf206bf24ff0d65502c6be2f58714
filip <fbielejec@gmail.com>
2019-08-19 22:05:40 +0200

    redeploy migration

--- migrations/8_redeploy_faucet_migration.js
+++ migrations/8_redeploy_faucet_migration.js
@@ -1,22 +1,46 @@
+const { linkBytecode } = require ("./utils.js");
+const { parameters } = require ('../truffle.js');
+
 const DankToken = artifacts.require("DankToken");
 const DankFaucet = artifacts.require ("DankFaucet");
+const dankTokenPlaceholder = "deaddeaddeaddeaddeaddeaddeaddeaddeaddead";
 
-// TODO : adjust these
-const dankTokenAddress = "0x4356ac01d00d3c5f689b93b8f6ea01a65973ed26";
-const dankFaucetAddress = "0x90920ee8ed813dbb4d78cecae87db75280ed0faf";
+// TODO : adjust addresses these
 
+// ganache
+const dankFaucetAddress = "";
+const dankTokenAddress = "";
+const dSGuardAddress = "";
+
+// ropsten
+// const dankFaucetAddress = "0x3182564B0aaB918C1B2994B004cE1B35572A7af0";
+// const dankTokenAddress = "0xeda9bf9199fab6790f43ee21cdce048781f58302";
+// const dSGuardAddress = "0xab4d684b2cc21ea99ee560a0f0d1490b09b09127";
+
+// mainnet
+// const dankFaucetAddress = "0x51605924b0c6e14f1bb3b73749675e22435896ac";
+// const dankTokenAddress = "0x0cb8d0b37c7487b11d57f1f33defa2b1d3cfccfe";
+// const dSGuardAddress = "0x5d0457f58ed4c115610a2253070a11fb82065403";
+
+/**
+ * This migration drains old faucet and redeploys it seeding new Faucet with ETH and DANK 
+ *
+ * Usage:
+ * truffle migrate --network ganache --reset --f 8 --to 8
+ */
 module.exports = function(deployer, network, accounts) {
 
   const address = accounts [0];
   const gas = 4e6;
   const opts = {gas: gas, from: address};
 
+  let balances = {};
+  
   deployer.then (() => {
     console.log ("@@@ using Web3 version:", web3.version.api);
     console.log ("@@@ using address", address);
   })
 
-  // TODO
   // get eth and dank balance
     .then (() => Promise.all ([
       DankFaucet.at (dankFaucetAddress),
@@ -33,8 +57,9 @@ module.exports = function(deployer, network, accounts) {
       ethBalance,
       dankBalance
     ]) => {
-      console.log ("@@@ Faucet ETH balance: ", ethBalance);
-      console.log ("@@@ Faucet DANK balance: ", dankBalance);
+      Object.assign (balances, {eth: ethBalance, dank: dankBalance})
+      console.log ("@@@ Faucet ETH balance: ", balances.eth);
+      console.log ("@@@ Faucet DANK balance: ", balances.dank);
     })
 
   // drain eth and dank
@@ -49,7 +74,19 @@ module.exports = function(deployer, network, accounts) {
     ]) => console.log ("@@@ Faucet succesfully drained of all funds"))
 
   // redeploy
-  // seed with the same eth and dank
-
-  ;
+    .then (() => {
+      linkBytecode(DankFaucet, dankTokenPlaceholder, dankTokenAddress);
+      return deployer.deploy(DankFaucet, parameters.dankFaucet.allotment, Object.assign(opts, {gas: 4000000}));          
+    })
+  
+  // seed with the same eth and dank amount
+    .then (() => Promise.all ([DankToken.at (dankTokenAddress),
+                               DankFaucet.at (dankFaucetAddress),
+                               DankFaucet.deployed()]))
+    .then (([dankToken,
+             dankFaucet,
+             newDankFaucet]) =>           
+           Promise.all ([
+             dankToken.transfer (newDankFaucet.address, balances.dank, Object.assign(opts, {gas: 200000})),
+             newDankFaucet.sendEth (Object.assign(opts, {gas: 200000, value: balances.eth}))]));
 }
