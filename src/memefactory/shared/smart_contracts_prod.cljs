46a82ea4c655d7d723cdeff72f16a18b8039f087
Filip Bielejec <fbielejec@gmail.com>
2019-11-05 08:25:52 +0100

    mainnet migration 10 (#653)

--- src/memefactory/shared/smart_contracts_prod.cljs
+++ src/memefactory/shared/smart_contracts_prod.cljs
@@ -21,7 +21,7 @@
     :address "0xb59ff0854090af74f4d56e9bc2d7e80e29a17b8d"},
    :param-change
    {:name "ParamChange",
-    :address "0x0d5fd7f02bc454f934c9ec448b265d7e7ef208f2"},
+    :address "0xd6734c56ba99db79d5388a7f4daaf1690ecde806"},
    :minime-token-factory
    {:name "MiniMeTokenFactory",
     :address "0x7cc607ba48f19ea817525c546cbec2bbae8d1499"},
@@ -33,14 +33,14 @@
     :address "0x92268d9c15657f14c9b0d551b97e260467dbe585"},
    :param-change-factory
    {:name "ParamChangeFactory",
-    :address "0xa59756a58c6c68fc56218db912639c380b16c90c"},
+    :address "0x44d28a7e9649ef2774540846255d9238e37c0e02"},
    :param-change-registry-fwd
    {:name "MutableForwarder",
     :address "0x942b6b83b654761b13fba7b230b9283ddec08f2c",
     :forwards-to :param-change-registry},
    :meme-factory
    {:name "MemeFactory",
-    :address "0xe44c530e11233b9cfa34ebbb9d9fc74ac852348d"},
+    :address "0xe9a45e9c0ac1c9393091c77a2ed0eb51f4f04f20"},
    :meme-token
    {:name "MemeToken",
     :address "0xd23043ce917ac39309f49dba82f264994d3ade76"},
@@ -51,7 +51,7 @@
    {:name "Registry",
     :address "0x8f24fb009e0ed2a342d56b5246ef69f72d297744"},
    :meme
-   {:name "Meme", :address "0x87a683ccd6e785c27d1a2708a4a380d58ef1a621"},
+   {:name "Meme", :address "0x1f342d5118176b6b01c7bf7a9f733d7b748b2431"},
    :meme-registry-fwd
    {:name "MutableForwarder",
     :address "0xe278b85a36f6b370347d69fb4744947e2965c058",
