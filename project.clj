c668ee81ad67d0ffd6b9945888761e2db39ddf61
Juan Monetta <jpmonettas@gmail.com>
2019-11-05 11:37:30 -0300

    Upgrade district-server-logging to 1.0.6, and killing travis

--- project.clj
+++ project.clj
@@ -32,7 +32,7 @@
                  [district0x/district-server-config "1.0.1"]
                  [district0x/district-server-db "1.0.4"]
                  [district0x/district-server-graphql "1.0.16"]
-                 [district0x/district-server-logging "1.0.5"]
+                 [district0x/district-server-logging "1.0.6"]
                  [district0x/district-server-middleware-logging "1.0.0"]
                  [district0x/district-server-smart-contracts "1.0.17"]
                  [district0x/district-server-web3 "1.0.1"]
