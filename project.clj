f389e5cc39f5d40a90fb82a41b28d25788b857e9
filip <fbielejec@gmail.com>
2019-11-12 10:46:04 +0100

    lib updates

--- project.clj
+++ project.clj
@@ -34,11 +34,9 @@
 
                  ;; TODO : update to new releases
                  [cljs-web3-next "0.0.15"]
-                 ;; [cljs-web3 "0.19.0-0-10"]
-                 ;; [district0x/district-server-smart-contracts "1.0.17"]
+                 [district0x/district-server-smart-contracts "1.1.0"]
                  [district0x/district-server-web3 "1.1.2"]
-                 ;; [district0x/district-server-web3-events "1.0.4"]
-                 ;; [district0x/district-server-web3-watcher "1.0.3"]
+                 [district0x/district-server-web3-events "1.1.0"]
 
                  [district0x/district-time "1.0.1"]
                  [district0x/district-ui-component-active-account "1.0.1"]
