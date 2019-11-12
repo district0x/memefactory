6f1d7d6edd4db4d487a4ff818af70ebe436ac0ec
filip <fbielejec@gmail.com>
2019-11-12 19:35:19 +0100

    WIP : tests

--- test/memefactory/tests/runner.cljs
+++ test/memefactory/tests/runner.cljs
@@ -1,32 +1,35 @@
 (ns memefactory.tests.runner
-  (:require [cljs-promises.async :refer-macros [<?]]
-            [cljs-promises.async]
-            [cljs-web3.core :as web3]
-            [cljs-web3.eth :as web3-eth]
-            [cljs.nodejs :as nodejs]
-            [clojure.core.async :as async :refer [<!]]
-            [district.graphql-utils :as graphql-utils]
-            [district.server.graphql :as graphql]
-            [district.server.graphql.utils :as utils]
-            [district.server.web3 :refer [web3]]
-            [doo.runner :refer-macros [doo-tests]]
-            [memefactory.server.contract.dank-token :as dank-token]
-            [memefactory.server.graphql-resolvers :refer [resolvers-map]]
-            [memefactory.shared.graphql-schema :refer [graphql-schema]]
-            [memefactory.tests.graphql-resolvers.graphql-resolvers-tests]
-            [memefactory.tests.smart-contracts.deployment-tests]
-            [memefactory.tests.smart-contracts.meme-auction-tests]
-            [memefactory.tests.smart-contracts.meme-tests]
-            [memefactory.tests.smart-contracts.param-change-tests]
-            [memefactory.tests.smart-contracts.registry-entry-tests]
-            [memefactory.tests.smart-contracts.registry-tests]
-            [memefactory.tests.smart-contracts.utils :as test-utils]
-            [taoensso.timbre :as log]))
+  (:require
+   [cljs-time.core :as time]
+   [cljs.nodejs :as nodejs]
+   [district.graphql-utils :as graphql-utils]
+   [district.server.db]
+   [district.server.graphql :as graphql]
+   [district.server.graphql.utils :as utils]
+   [district.server.middleware.logging :refer [logging-middlewares]]
+   [district.server.web3 :refer [web3]]
+   [district.shared.async-helpers :as async-helpers]
+   [doo.runner :refer-macros [doo-tests]]
+   [memefactory.server.graphql-resolvers :refer [resolvers-map]]
+   [memefactory.shared.graphql-schema :refer [graphql-schema]]
+   [memefactory.shared.smart-contracts-dev :refer [smart-contracts]]
+   [memefactory.tests.graphql-resolvers.graphql-resolvers-tests]
+   [memefactory.tests.smart-contracts.utils :as test-utils]
+   [mount.core :as mount]
+   [taoensso.timbre :as log]
+
+   ;; [memefactory.tests.smart-contracts.deployment-tests]
+   ;; [memefactory.tests.smart-contracts.meme-auction-tests]
+   ;; [memefactory.tests.smart-contracts.meme-tests]
+   ;; [memefactory.tests.smart-contracts.param-change-tests]
+   ;; [memefactory.tests.smart-contracts.registry-entry-tests]
+   ;; [memefactory.tests.smart-contracts.registry-tests]
+
+   ))
 
 (nodejs/enable-util-print!)
-(cljs-promises.async/extend-promises-as-pair-channels!)
 
-(set! (.-error js/console) (fn [x] (.log js/console x)))
+(async-helpers/extend-promises-as-channels!)
 
 (defn on-jsload []
   (graphql/restart {:schema (utils/build-schema graphql-schema
@@ -36,19 +39,38 @@
                     :field-resolver (utils/build-default-field-resolver graphql-utils/gql-name->kw)}))
 
 (defn start-and-run-tests []
-  (async/go
-    ((test-utils/create-before-fixture))
-    (log/info "Account balances now are " ::start-and-run-tests)
-    (doseq [acc (web3-eth/accounts @web3)]
-      (println (str "Balance of " acc " is " (<? (dank-token/balance-of acc)))))
-    (log/info "Running tests" ::start-and-run-tests)
-    (cljs.test/run-tests
-     'memefactory.tests.graphql-resolvers.graphql-resolvers-tests
-     'memefactory.tests.smart-contracts.registry-entry-tests
-     'memefactory.tests.smart-contracts.meme-tests
-     'memefactory.tests.smart-contracts.meme-auction-tests
-     'memefactory.tests.smart-contracts.registry-tests
-     ;; 'memefactory.tests.smart-contracts.param-change-tests
-     )))
+  (-> (mount/with-args {:web3 {:url "ws://127.0.0.1:8545"}
+                        :db {:opts {:memory true}}
+                        :smart-contracts {:contracts-var #'smart-contracts}
+                        :graphql {:port 6400
+                                  :middlewares [logging-middlewares]
+                                  :schema (utils/build-schema graphql-schema
+                                                              resolvers-map
+                                                              {:kw->gql-name graphql-utils/kw->gql-name
+                                                               :gql-name->kw graphql-utils/gql-name->kw})
+                                  :field-resolver (utils/build-default-field-resolver graphql-utils/gql-name->kw)
+                                  :path "/graphql"
+                                  :graphiql true}
+                        :logging {:level :info
+                                  :console? true}
+                        :time-source :blockchain
+                        :ranks-cache {:ttl (time/in-millis (time/minutes 60))}})
+      (mount/only [#'district.server.logging/logging
+                   #'district.server.db/db
+                   #'memefactory.server.db/memefactory-db
+                   #'district.server.graphql/graphql
+                   #'district.server.web3/web3
+                   #'district.server.smart-contracts/smart-contracts])
+      (mount/start)
+      (as-> $ (log/warn "Started" $)))
+
+  (doo-tests
+   'memefactory.tests.graphql-resolvers.graphql-resolvers-tests
+   ;; 'memefactory.tests.smart-contracts.registry-entry-tests
+   ;; 'memefactory.tests.smart-contracts.meme-tests
+   ;; 'memefactory.tests.smart-contracts.meme-auction-tests
+   ;; 'memefactory.tests.smart-contracts.registry-tests
+   ;; 'memefactory.tests.smart-contracts.param-change-tests
+   ))
 
 (start-and-run-tests)
