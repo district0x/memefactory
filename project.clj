e86c06b2692655e7fca9ecccb0bc20573f79fd65
madvas <matus.lestan@gmail.com>
2019-11-21 17:01:58 +0700

    update district-server-web3-events

--- project.clj
+++ project.clj
@@ -36,7 +36,7 @@
                  [district0x/district-server-middleware-logging "1.0.0"]
                  [district0x/district-server-smart-contracts "1.0.17"]
                  [district0x/district-server-web3 "1.0.1"]
-                 [district0x/district-server-web3-events "1.0.4"]
+                 [district0x/district-server-web3-events "1.0.5"]
                  [district0x/district-server-web3-watcher "1.0.3"]
                  [district0x/district-time "1.0.1"]
                  [district0x/district-ui-component-active-account "1.0.1"]
