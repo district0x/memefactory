9fb259f0de7025f5ef7a8a9b4dc38aa098e7b640
Peter Chung <touhonoob@gmail.com>
2021-06-28 20:43:47 +0800

    add share-buttons to memefolio page

--- src/memefactory/styles/component/share_buttons.clj
+++ src/memefactory/styles/component/share_buttons.clj
@@ -1,20 +1,20 @@
 (ns memefactory.styles.component.share-buttons
   (:require [garden.def :refer [defstyles]]
-            [garden.units :refer [px,em]]
+            [garden.units :refer [px, em]]
             [memefactory.styles.base.media :refer [for-media-max]]))
 
 
 (defstyles core
-  [:.share-buttons
-   {:position "absolute"
-    :top   (em 2)
-    :right  (em 2)}
+  [:.meme-detail
+   [:.share-buttons
+    {:position "absolute"
+     :top      (em 2)
+     :right    (em 2)}
 
-   (for-media-max :tablet
-                  [:&
-                   {:top "auto" :right "auto" :bottom (em 2), :left (em 2)}])]
-  [:.share-buttons-bottom-container
-   (for-media-max :tablet
-                  [:&
-                   {:display "block" :height (px 32) :margin-top (em 2)}])
-   ])
+    (for-media-max :tablet
+                   [:&
+                    {:top "auto" :right "auto" :bottom (em 2), :left (em 2)}])]
+   [:.share-buttons-bottom-container
+    (for-media-max :tablet
+                   [:&
+                    {:display "block" :height (px 32) :margin-top (em 2)}])]])
